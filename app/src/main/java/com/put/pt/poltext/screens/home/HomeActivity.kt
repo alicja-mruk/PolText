9510321f166af9f70afb666cad58357f0ef7f1f7
Alicja Mruk <alicja.mruk@acaisoft.com>
2021-05-02 14:35:11 +0200

    Add toolbar to home activity

--- app/src/main/java/com/put/pt/poltext/screens/home/HomeActivity.kt
+++ app/src/main/java/com/put/pt/poltext/screens/home/HomeActivity.kt
@@ -1,24 +1,32 @@
 package com.put.pt.poltext.screens.home
 
+
 import android.os.Bundle
-import com.put.pt.poltext.data.firebase.common.auth
+import androidx.navigation.fragment.NavHostFragment
+import com.put.pt.poltext.R
 import com.put.pt.poltext.databinding.ActivityHomeBinding
 import com.put.pt.poltext.screens.BaseActivity
+
 
 class HomeActivity : BaseActivity() {
 
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.logout.setOnClickListener {
-            auth.signOut()
-        }
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
+        bindOnClickListeners()
+    }
 
+    private fun bindOnClickListeners () {
+        binding.include.settingsBtn.setOnClickListener {
+            navHostFragment.navController.navigate(R.id.action_chatPrivateFragment_to_settingsFragment)
+        }
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/put/pt/poltext/screens/home/private_chat/ChatPrivateFragment.kt
@@ -0,0 +1,26 @@
+package com.put.pt.poltext.screens.home.private_chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.put.pt.poltext.databinding.FragmentChatPrivateBinding
+
+class ChatPrivateFragment : Fragment() {
+    private var _binding: FragmentChatPrivateBinding ? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentChatPrivateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
