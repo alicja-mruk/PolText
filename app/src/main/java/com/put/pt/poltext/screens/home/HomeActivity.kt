354aa9b0b7125c404a41b9cda4de42e05bb4617d
Alicja Mruk <alicja.mruk@acaisoft.com>
2021-05-02 17:11:04 +0200

    Add logic to send message on public channel

--- app/src/main/java/com/put/pt/poltext/screens/home/HomeActivity.kt
+++ app/src/main/java/com/put/pt/poltext/screens/home/HomeActivity.kt
@@ -6,21 +6,21 @@ import androidx.navigation.NavController
 
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.put.pt.poltext.R
 import com.put.pt.poltext.databinding.ActivityHomeBinding
 import com.put.pt.poltext.screens.BaseActivity
+import com.put.pt.poltext.screens.auth.register.RegisterViewModel
+import com.put.pt.poltext.screens.home.public_chat.ChatPublicFragment
 
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : BaseActivity(), ChatPublicFragment.Listener {
 
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var navController: NavController
     private lateinit var navHostFragment: NavHostFragment
+    private lateinit var chatViewModel: ChatViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +29,9 @@ class HomeActivity : BaseActivity() {
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
         navController = findNavController(R.id.nav_host_fragment_home)
-
         NavigationUI.setupWithNavController( binding.bottomNavView, navController)
 
+        chatViewModel = initViewModel()
         bindOnClickListeners()
     }
 
@@ -40,6 +40,10 @@ class HomeActivity : BaseActivity() {
             navController.navigateUp();
             navController.navigate(R.id.settingsFragment)
         }
+    }
+
+    override fun onSendMessage(message: String) {
+        chatViewModel.onSendMessage(message)
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/put/pt/poltext/screens/home/public_chat/ChatPublicFragment.kt
+++ app/src/main/java/com/put/pt/poltext/screens/home/public_chat/ChatPublicFragment.kt
@@ -1,19 +1,44 @@
 package com.put.pt.poltext.screens.home.public_chat
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.put.pt.poltext.databinding.FragmentChatPublicBinding
-
 
 class ChatPublicFragment : Fragment() {
     private var _binding: FragmentChatPublicBinding ? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var mListener: Listener
+
+    interface Listener {
+        fun onSendMessage(message: String)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        bindOnClickListeners()
+    }
+
+    private fun bindOnClickListeners(){
+        binding.sendBtn.setOnClickListener{
+            val text = binding.message.text.toString()
+            mListener.onSendMessage(text)
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mListener = context as Listener
     }
 
     override fun onCreateView(
