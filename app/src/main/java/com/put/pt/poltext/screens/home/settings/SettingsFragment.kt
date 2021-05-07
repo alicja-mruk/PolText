e8071b189c2fd497011fd05e12e00e799f06c30c
Alicja Mruk <alicja.mruk@acaisoft.com>
2021-05-08 01:59:54 +0200

    Add settings fragment screen with logout button

--- app/src/main/java/com/put/pt/poltext/screens/home/settings/SettingsFragment.kt
+++ app/src/main/java/com/put/pt/poltext/screens/home/settings/SettingsFragment.kt
@@ -1,19 +1,50 @@
 package com.put.pt.poltext.screens.home.settings
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.put.pt.poltext.databinding.FragmentSettingsBinding
+import com.put.pt.poltext.screens.auth.login.LoginActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-
+@ExperimentalCoroutinesApi
 class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding ? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private val viewModel by viewModel<SettingsViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initializeFragment()
+    }
+
+    private fun initializeFragment(){
+        bindOnClickListeners()
+        observeLiveData()
+    }
+
+    private fun bindOnClickListeners(){
+        binding.logoutBtn.setOnClickListener {
+            viewModel.logout()
+        }
+    }
+
+    private fun observeLiveData(){
+        with(viewModel){
+            goToLoginScreen.observe(viewLifecycleOwner, {
+                goToLoginScreen()
+            })
+        }
+    }
+    
+    private fun goToLoginScreen(){
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onCreateView(
