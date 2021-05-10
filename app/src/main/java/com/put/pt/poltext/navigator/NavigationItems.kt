7354373424deb3a4088a8f1405d4043cf883261c
Alicja Mruk <alicja.mruk@acaisoft.com>
2021-05-10 21:39:52 +0200

    Fix typo

--- app/src/main/java/com/put/pt/poltext/navigator/NavigationItems.kt
+++ app/src/main/java/com/put/pt/poltext/navigator/NavigationItems.kt
@@ -6,7 +6,7 @@ class NavigationItems {
     companion object {
         val data = arrayListOf(
             NavigationItemModel(R.drawable.ic_chat_private, "Private Chat"),
-            NavigationItemModel(R.drawable.ic_chat_public, "Pubic Chat"),
+            NavigationItemModel(R.drawable.ic_chat_public, "Public Chat"),
             NavigationItemModel(R.drawable.ic_profile, "Profile"),
         )
     }
